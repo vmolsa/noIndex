7bd46f4c0640b4cbc65f85c5e0be0df5514c0a25
Ville Mölsä <ville.molsa@gmail.com>
2015-09-27 14:32:11 +0300

    Update plain memory db

--- drivers/memory.js
+++ drivers/memory.js
@@ -5,67 +5,67 @@ var noIndex = require('../lib/noIndex.js');
 var db = {};
 
 var config = {
-  get: function(resolve, reject, driver, key) {
-	  if (db.key) {
-      return resolve(db.key);
+  get: function(resolve, reject, driver, key, options) {   
+	  if (db[key]) {
+      return resolve(db[key]);
     }
-    
+
     reject(null);
   },
-  set: function(resolve, reject, driver, key, value) {   
+  set: function(resolve, reject, driver, key, value, options) {   
     if (value) {
-      db.key = value;
+      db[key] = value;
       resolve(value);
     }
     
     reject(null);
   },
-  del: function(resolve, reject, driver, key) {
-    var value = db.key;
+  del: function(resolve, reject, driver, key, options) {
+    var value = db[key];
     
     if (value) {
-	    delete db.key;
+	    delete db[key];
       return resolve(value);
     }
     
     reject(null);
   },
-  hget: function(resolve, reject, driver, key, hash) {
-	  if (db.key) {
-      var entry = db.key;
+  hget: function(resolve, reject, driver, key, hash, options) {
+	  if (db[key]) {
+      var entry = db[key];
       
-      if (entry.hash) {
-        return resolve(entry.hash);
+      if (entry[hash]) {
+        return resolve(entry[hash]);
       }
     }
     
     reject(null);
   },
-  hset: function(resolve, reject, driver, key, hash, value) {
+  hset: function(resolve, reject, driver, key, hash, value, options) {
     if (value) {
-      if (!db.key) {
-        db.key = {};
+      if (!db[key]) {
+        db[key] = {};
       }
       
-      db.key.hash = value;
+      db[key].hash = value;
       resolve(value);
     }
     
     reject(null);
   }, 
-  hdel: function(resolve, reject, driver, key, hash) {
+  hdel: function(resolve, reject, driver, key, hash, options) {
     var value = null;
     
-    if (db.key) {
-      var entry = db.key;  
+    if (db[key]) {
+      var entry = db[key];  
       
-      if (entry.hash) {
-        value = entry.hash;
-	      delete entry.hash;
+      if (entry[hash]) {
+        value = entry[hash];
+	      delete entry[hash];
       }
       
       if (!Object.keys(entry).length) {
-        delete db.key;
+        delete db[key];
       }
     }
     
